65505b0485373b0b993ee7e8a0490aaaccc4eae1
陈乐杰 <1946363509@qq.com>
2018-07-30 19:52:41 +0800

    init

---
+++ BaiduMapDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  BaiduMapDemo
+//
+//  Created by 陈乐杰 on 2018/7/30.
+//  Copyright © 2018年 nst. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
